9b0c7812d49458001719b3bac29a45e221e3fcf1
Stroupe79 <cstroupe79@gmail.com>
2019-05-03 13:34:47 -0500

    Finished! Everything done and working

--- app.js
+++ app.js
@@ -75,10 +75,18 @@ inquirer
     // * Edit the text in random.txt to test out the feature for search-movies and search-concerts.
 
 function omdb(search){
-    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=e5d832e9";
+    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=e5d832e9&tomatoes=true";
     axios.get(queryUrl).then(
         function (response){
+            // console.log(response)
+            console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
         }
     )
 }
@@ -88,8 +96,11 @@ function musicApi(search){
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-       
-      console.log(data); 
+    //   console.log(data.tracks.items[0]); 
+      console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+      console.log("Name: " + data.tracks.items[0].name); 
+    console.log("Album: " + data.tracks.items[0].album.name); 
+      console.log("Preview Link: " + data.tracks.items[0].preview_url); 
       });
 }
 
@@ -97,13 +108,13 @@ function musicApi(search){
 function tm(search){
     const tmKey="&apikey=rrFQUi7azSu6BIs8pNUwk9tDZHSTv8YY"
     var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword=" + search + tmKey;
-    console.log(queryUrl)
     axios.get(queryUrl).then(
         function (response){
-            console.log(response)
-            console.log("Event Name: " + response._embedded.events[0].name);
-            console.log("Start Date: " + response._embedded.events[0].dates.start.localTime)
-            console.log("Venue: " + response._embedded.events[0]._embedded.venues[0].name)
+            // console.log("Venue: " + response._embedded.venues[0].name)
+            console.log("Event Name: " + response.data._embedded.events[0].name);
+            console.log("Venue: " + response.data._embedded.events[0]._embedded.venues[0].name)
+            console.log("Venue City: " + response.data._embedded.events[0]._embedded.venues[0].city.name)
+            console.log("Start Date: " + response.data._embedded.events[0].dates.start.localTime)
         }
     )
 }
